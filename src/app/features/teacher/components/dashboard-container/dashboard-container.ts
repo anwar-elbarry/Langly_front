1e57e5c03539fbd976cdb997b4763433e6cead29
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-28 15:30:06 +0000

    add dashboard-container component and its associated files for admin, student, superAdmin, and teacher features

---
+++ src/app/features/teacher/components/dashboard-container/dashboard-container.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-container.html',
+  styleUrl: './dashboard-container.css',
+})
+export class DashboardContainer {
+
+}
