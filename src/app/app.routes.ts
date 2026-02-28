61fc26f1eafa18a97a8f0ea6e770cd246e2c1707
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-28 15:29:04 +0000

    add routing configuration for authentication and user roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/components/login/login').then(m => m.Login)
+  },
+  {
+    path:'superAdmin',
+    loadComponent:() => import('./features/superAdmin/components/dashboard-container/dashboard-container').then(m => m.DashboardContainer)
+  },
+  {
+    path:'schoolAdmin',
+    loadComponent:() => import('./features/admin/components/dashboard-container/dashboard-container').then(m => m.DashboardContainer)
+  },
+  {
+    path:'teacher',
+    loadComponent:() => import('./features/teacher/components/dashboard-container/dashboard-container').then(m => m.DashboardContainer)
+  },
+  {
+    path:'student',
+    loadComponent:() => import('./features/student/components/dashboard-container/dashboard-container').then(m => m.DashboardContainer)
+  }
+];
