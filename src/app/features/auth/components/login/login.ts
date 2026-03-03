099dcd8b936fc1c2e56cb4f17e2dedcaf16aa4df
anouar eil barry <elbarryanwar37@gmail.com>
2026-03-03 22:17:51 +0000

    feat: refactor login form to use shared components and improve error handling

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -3,23 +3,47 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Store } from '@ngrx/store';
 import { AuthPage } from '../../../../core/store/actions/auth.actions';
 import { AuthRequest } from '../../models/Auth.request';
+import { FormFieldComponent } from '../../../../shared/ui/form-field/form-field';
+import { InputComponent } from '../../../../shared/ui/input/input';
+import { ButtonComponent } from '../../../../shared/ui/button/button';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { selectAuthLoading } from '../../../../core/store/selectors/auth.selectors';
+import { SpinnerComponent } from "../../../../shared/ui/spinner/spinner";
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormFieldComponent, InputComponent, ButtonComponent, SpinnerComponent],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
   private store = inject(Store);
-
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   });
 
+  isLoading = toSignal(this.store.select(selectAuthLoading));
+
   showPassword = false;
+
+  get emailError(): string {
+    const control = this.form.get('email');
+    if (control?.touched && control?.invalid) {
+      if (control.hasError('required')) return 'Email is required';
+      if (control.hasError('email')) return 'Please enter a valid email address';
+    }
+    return '';
+  }
+
+  get passwordError(): string {
+    const control = this.form.get('password');
+    if (control?.touched && control?.hasError('required')) {
+      return 'Password is required';
+    }
+    return '';
+  }
 
   togglePassword() {
     this.showPassword = !this.showPassword;
